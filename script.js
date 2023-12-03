953d23440a56b8a0a142917a5358d931256fda86
CHXSEEE <152750298+CHXSEEE@users.noreply.github.com>
2023-12-03 07:27:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const wrapper = document.querySelector(".wrapper"); const question = document.querySelector(".question");
+const gif = document.querySelector(".gif") const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn"); yesBtn.addEventListener("click",
+() => { question.innerHTML = "Aaaaa, I like you too"; gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+}); noBtn.addEventListener("mouseover", () => { const noBtnRect = noBtn.getBoundingClientRect();
+const maxX = window.innerWidth - noBtnRect.width; const maxY = window.innerHeight
+- noBtnRect.height; const randomX = Math.floor(Math.random() * maxX); const randomY
+= Math.floor(Math.random() * maxY); noBtn.style.left = randomX + "px"; noBtn.style.top
+= randomY + "px"; });
+<!--
+Visual Code Mobile
+Developed By Manish Nirmal
+App Available on Play Store :-
+https://play.google.com/store/apps/details?id=lk.visual.code.mobile
+YouTube :-
+https://youtube.com/ManishNirmal
+-->
